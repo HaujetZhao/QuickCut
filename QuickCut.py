5f6cb58ba1ba9a21aeb068ff44052c551edf6f94
shniubobo <shniubobo@outlook.com>
2020-08-04 16:22:07 +0800

    fix decoding errors of chinese characters in ffmepg commands
    修复ffmpeg指令里中文字符的编码问题
    
    Fix #24

--- QuickCut.py
+++ QuickCut.py
@@ -4745,18 +4745,18 @@ class CommandThread(QThread):
                                                 stderr=subprocess.STDOUT, startupinfo=subprocessStartUpInfo)
             else:
                 self.process = subprocess.Popen(self.command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
-                                                universal_newlines=True, encoding='utf-8', start_new_session=True)
+                                                start_new_session=True)
         except:
             self.print(self.tr('出错了，本次运行的命令是：\n\n%s\n\n你可以将上面这行命令复制到 cmd 窗口运行下，看看报什么错，如果自己解决不了，把那个报错信息发给开发者。如果是 you-get 和 youtube-dl 的问题，请查看视频教程：https://www.bilibili.com/video/BV18T4y1E7FF?p=5\n\n') % self.command)
         try:
-            try:
-                wrapper = io.TextIOWrapper(self.process.stdout, encoding='utf-8')
-                for line in wrapper:
-                    self.printForFFmpeg(line)
-            except:
-                wrapper.reconfigure(encoding='gbk')
-                for line in wrapper:
-                    self.printForFFmpeg(line)
+            while True:
+                line = self.process.stdout.readline()
+                if not line:
+                    break
+                try:
+                    self.printForFFmpeg(line.decode('utf-8'))
+                except UnicodeDecodeError:
+                    self.printForFFmpeg(line.decode('gbk'))
         except:
             self.print(
                 self.tr('''出错了，本次运行的命令是：\n\n%s\n\n你可以将上面这行命令复制到 cmd 窗口运行下，看看报什么错，如果自己解决不了，把那个报错信息发给开发者\n''') % self.command)
